d8083b0650893cae5154bfef9d297b195113f3ef
YAZID <y421d.ben@gmail.com>
2023-04-22 15:05:50 +0100

    1MIAD/ES/C++/Infrence engine chainage avant

---
+++ 1MIAD/Connectionist Method/Neural Networks/TP05.py
@@ -0,0 +1,101 @@
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+
+Table = []
+
+def fill ():
+    for indx in range(100):
+        X.append([0] * 3)
+        Y.append([0] * 1)
+        YP.append([0] * 1)
+    W.append([0] * 1 )
+    W.append([1] * 1 )
+    W.append([2] * 1 )
+    W[0] = float(random.random())
+    W[1] = float(random.random())
+    W[2] = float(random.random())
+    for i in range (100):
+        X[i][0] = 1
+        X[i][1] = random.random()
+        X[i][2] = random.random()
+        if (i<=50):
+            Y[i] = 0
+        else:
+            Y[i] = 1
+
+def Sigmoide(IN):
+    return 1/(1+np.exp(-IN))
+
+def learn(X,Y,w,e,dw,n) :
+    # for i in range(n) : # répétons n fois
+    w = descend(X,Y,w,e,dw) # effectuons un pas
+    LW.append(L(X,Y,w))
+    print(L(X,Y,w)) # imprimons la valeur du coût
+    return w # on renvoie le vecteur de poids
+    
+def descend(X,Y,w,e,dw) :
+    g = gradient(X,Y,w,dw) # calcul du vecteur de gradient
+    w[0] = w[0] - e*g[0] # mise à jour de w[0]
+    w[1] = w[1] - e*g[1] # mise à jour de w[1]
+    return w # on renvoie le nouveau vecteur de paramètres
+
+def gradient(X,Y,w,dw) :
+    h = L(X,Y,w) # calcul du coût
+    wa = [0,0] # crée un vecteur wa
+    wa[0] = w[0] + dw # perturbation de la 1re coordonnée
+    wa[1] = w[1]
+    a = L(X,Y,wa) # calcul du coût après perturbation
+    wb = [0,0] # crée un vecteur wb
+    wb[0] = w[0]
+    wb[1] = w[1] + dw # perturbation de la 2e coordonnée
+    b = L(X,Y,wb) # calcul du coût après perturbation
+    g = [0,0] # crée un vecteur g
+    g[0] = (a-h)/dw # pente dans la 1re coordonnée
+    g[1] = (b-h)/dw # pente dans la 2e coordonnée
+    return g # retourne le vecteur de gradient
+
+
+def L(X,Y,w) :
+    s = 0
+    for i in range(0,2) :
+        # print(i)
+        s = s + pow(Y[i]-w[i],2)
+    return s/2
+
+
+# /////////////////   PROGRAMME PRINCIPAL    //////////////////////////
+# X : Tableau des entrées
+# Y : Tableau des sorties désirées
+# W : vecteur des poids
+# e : Pas du gradient
+# dw : Perturbation
+# YP : Tabelau des sorties calculées
+X = []
+Y = []
+YP = []
+LW=[]
+W=[]
+fill()
+e=0.01
+dw=0.01
+n=10
+print("Winitial[ 1 , 2 , 3 ]")
+print(W)
+for epoch in range(n):
+    print(f'============Epoch {epoch+1}=====================')
+    for i in range(len(X)):
+        input_ = X[i][0] * W[0] + X[i][1] * W[1] + X[i][2] * W[2]
+        output = Sigmoide(input_)
+        YP[i] = output
+        error = Y[i] - output
+    T = learn (X,Y,W,e,dw,n)
+print("Affichage des poids optimals :")
+print("Wfinal[ 0, 1,  2  ]:")
+print(T)
+#PLot the LW with Y sacle as symlog
+plt.plot(LW)
+plt.show()
+
+
+
--- 1MIAD/Expert System/C++/Inference engine/main.cc
+++ 1MIAD/Expert System/C++/Inference engine/main.cc
@@ -11,6 +11,11 @@ struct Rule {
     vector<string> premisses;
     vector<string> conclusions;
 };
+struct Fact{
+    string name;
+};
+
+bool Chainage_avant_profonder(vector<Rule>& rules, string goal,vector<Fact>&facts);
 
 int main() {
     vector<Rule> rules;
@@ -22,7 +27,7 @@ int main() {
     }
 
     string line;
-    int i =0 ;
+    int i = 0;
     while (getline(reglestxt, line)) {
         Rule rule;
         size_t rule_mid = line.find(">");
@@ -33,12 +38,12 @@ int main() {
             continue;
         }
 
-        i+=1;
-        rule.name = "R"+to_string(i);
+        i += 1;
+        rule.name = "R" + to_string(i);
         rule.active = true;
 
-        string premissesStr = line.substr(0, (rule_mid-1));
-        string conclusionsStr = line.substr(rule_mid+2,(rule_end-rule_mid)-2);
+        string premissesStr = line.substr(0, (rule_mid - 1));
+        string conclusionsStr = line.substr(rule_mid + 2, (rule_end - rule_mid) - 2);
 
         size_t start = 0, end = 0;
         for (int i = 0; i < premissesStr.length(); i++) {
@@ -74,4 +79,101 @@ int main() {
         }
         cout << "| Active : " << (rules[i].active ? "True" : "False") << endl;
     }
+
+    vector<Fact> facts;
+    int nbFacts;
+    cout << "Combien du faits ya t'il? "<< endl;
+    cin >> nbFacts;
+    cin.ignore();
+    Fact fact;
+    for (int i = 0; i < nbFacts; i++) {
+        cout << "Entrez le nom du fait : ";
+        getline(cin, fact.name);
+        facts.push_back(fact);
+    }
+    string goal;
+    cout << "Entrez le but à atteindre : ";
+    getline(cin, goal);
+
+    if (Chainage_avant_profonder(rules, goal,facts)) {
+        cout << "Le but a été atteint." << endl;
+    } else {
+        cout << "Le but n'a pas pu être atteint." << endl;
+    }
+}
+bool Chainage_avant_profonder(vector<Rule>& rules, string goal, vector<Fact>&facts){
+    bool goalReached = false;
+    bool allRulesInactive = false;
+    int c=0;
+    
+    // Tant que le but n'est pas atteint et qu'il y a des règles actives à traiter
+    while (!goalReached && !allRulesInactive) {
+        cout << "================= Cycle " << c+1<< endl;
+        bool ruleFired = false;
+        allRulesInactive = true;
+        // Parcourir toutes les règles
+        for (int i = 0; i < rules.size(); i++) {
+            // Si la règle est active
+            if (rules[i].active) {
+                bool allPremissesTrue = true;
+                // Parcourir toutes les prémisses de la règle
+                for (int j = 0; j < rules[i].premisses.size(); j++) {
+                    bool premissTrue = false;
+                    // Parcourir tous les faits
+                    for (int k = 0; k < facts.size(); k++) {
+                        // Si la prémisses est présente dans la base de faits
+                        if (rules[i].premisses[j] == facts[k].name) {
+                            premissTrue = true;
+                            break;
+                        }
+                    }
+                    if (!premissTrue) {
+                        allPremissesTrue = false;
+                        break;
+                    }
+                }
+                // Si toutes les prémisses sont vraies, on ajoute les conclusions à la base de faits et on désactive la règle
+                if (allPremissesTrue) {
+                    // PRINT LA REGLE A APPLIQUE
+                    cout << "Règle appliquée : " << rules[i].name << endl;
+                    ruleFired = true;
+                    for (int j = 0; j < rules[i].conclusions.size(); j++) {
+                        bool factExists = false;
+                        for (int k = 0; k < facts.size(); k++) {
+                            if (rules[i].conclusions[j] == facts[k].name) {
+                                factExists = true;
+                                break;
+                            }
+                        }
+                        if (!factExists) {
+                            Fact newFact;
+                            newFact.name = rules[i].conclusions[j];
+                            facts.push_back(newFact);
+                            // print full updated fact vector
+                            cout << "Base de faits : {";
+                            for (int i = 0; i < facts.size(); i++) {
+                                cout << facts[i].name <<",";
+                            }
+                            cout << "}" << endl;
+                        }
+                    }
+                    rules[i].active = false;
+                    allRulesInactive = false;
+                    break;
+                }
+            }
+        }
+        if (!ruleFired) {
+            allRulesInactive = true;
+        }
+        // Vérifier si le but a été atteint
+        for (int i = 0; i < facts.size(); i++) {
+            if (facts[i].name == goal) {
+                goalReached = true;
+                break;
+            }
+        }
+        c++ ;
+    }
+    return goalReached;
 }
